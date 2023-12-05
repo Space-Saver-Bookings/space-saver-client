71ad546fbb1470dc87b1ac9da3eacfb78a0982f3
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-06 09:19:16 +1100

    feat: initialised & tested mui

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,39 @@
+import { Avatar } from "@mui/material"
+
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+
 function Header() {
     return (
         <div className="font-coplette col-start-2 col-span-full">
             <h1>Good %timeOfDay%, %firstName%</h1>
+            <Avatar {...stringAvatar('Alex Holder')}/>
         </div>
     )
 }
