d445906c2686ea4612d11b4c6259414b124ad398
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-06 10:18:21 +1100

    feat: implemented sidebar & main section

--- src/layout/MainSection.jsx
+++ src/layout/MainSection.jsx
@@ -1,6 +1,6 @@
 function MainSection() {
     return (
-        <div className=" col-start-2 col-span-full row-start-2 row-span-full">
+        <div className=" col-start-2 col-span-full row-start-2 row-span-full p-5">
             <h2>Main</h2>
         </div>
     )
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,8 +1,15 @@
+import desktopLogo from '../assets/logo/ss-logo.png';
+
 function Sidebar() {
   return (
-    <div className="col-span-1 col-end-2 row-start-1 row-span-full">
-      <h2>Sidebar</h2>
-    </div>
+    <aside className="col-span-1 col-end-2 row-span-full row-start-1 border-r-2 flex flex-col items-center">
+      <img
+        src={desktopLogo}
+        alt="SpaceSaver desktop logo"
+        className="mt-[-3rem]"
+      />
+      <div className='border mt-[-1.5rem] w-[13rem]'></div>
+    </aside>
   );
 }
 
