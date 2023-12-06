0b165a18e9d0d2dfdb2297739f47df51a0f1f13b
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-06 23:09:27 +1100

    fix: viewport size breaking grid layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <section className="sm:grid-cols-23 grid h-full sm:grid-rows-18 gap-6 rounded-md bg-blue-300">
+    <section className="sm:grid-cols-23 grid h-full sm:grid-rows-18 gap-6">
       <div className="col-start-1 col-end-8 row-start-1 row-end-[7] rounded-xl bg-yellow-200">
         Book again
       </div>
@@ -13,10 +13,10 @@ function Home() {
       <div className="col-start-1 col-end-[15] row-start-7 row-end-[14] rounded-xl bg-yellow-200">
         Upcoming bookings
       </div>
-      <div className="col-start-1 col-end-[15] row-span-5 flex justify-between rounded-xl bg-yellow-200">
-        <div className="w-[12rem] rounded-xl bg-blue-200">rooms in use</div>
-        <div className="w-[12rem] rounded-xl bg-blue-200">users in rooms</div>
-        <div className="w-[12rem] rounded-xl bg-blue-200">Most used rooms</div>
+      <div className="col-start-1 col-end-[15] row-span-5 grid grid-cols-3 gap-8">
+        <div className="rounded-xl bg-blue-200">rooms in use</div>
+        <div className="rounded-xl bg-blue-200">users in rooms</div>
+        <div className="rounded-xl bg-blue-200">Most used rooms</div>
       </div>
     </section>
   );
