5f023943db34b3e7670786b23fdbfa67d3f891b9
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-09 15:35:06 +1100

    feat: implemented space layout & cards

--- src/pages/Spaces.jsx
+++ src/pages/Spaces.jsx
@@ -1,9 +1,33 @@
+import DashItem from '../components/dashboard/DashItem';
+
+const spaces = Array.from(Array(4), (_, idx) => `Space ${idx + 1}`);
+
 function Spaces() {
-    return (
-        <div>
-            <h3>Spaces</h3>
-        </div>
-    )
+  return (
+    <section className="flex flex-col gap-6">
+      <div className="flex justify-center">
+        <button>Joined Spaces</button>
+      </div>
+
+      <section className="flex flex-wrap gap-5">
+        {spaces.map((space) => (
+          <DashItem
+            key={space}
+            styling="w-[20rem] h-[14.5rem]"
+            heading={space}
+            headingStyling="self-center my-auto"
+          />
+        ))}
+
+        <DashItem
+          styling="w-[20rem] h-[14.5rem]"
+          heading="Join Space +"
+          bgColor="bg-slate-300"
+          headingStyling="self-center my-auto"
+        />
+      </section>
+    </section>
+  );
 }
 
-export default Spaces
+export default Spaces;
