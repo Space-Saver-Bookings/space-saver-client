f6a8a214243443f4219c91e8c98a50f43e735d44
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-09 16:03:37 +1100

    feat: implemented toggle joined/owned

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
 
-function Button({children}) {
-  return <button className='bg-white py-2 px-4 rounded-lg border border-gray-300 shadow-2xl'>{children}</button>;
+function Button({children, onClick}) {
+  return (
+    <button
+      className="rounded-lg border border-gray-300 bg-white px-4 py-2 shadow-2xl"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
   children: PropTypes.node,
+  onClick: PropTypes.func,
 };
 
 export default Button;
--- src/pages/Spaces.jsx
+++ src/pages/Spaces.jsx
@@ -1,13 +1,22 @@
+import {useState} from 'react';
 import Button from '../components/buttons/Button';
 import DashItem from '../components/dashboard/DashItem';
 
 const spaces = Array.from(Array(4), (_, idx) => `Space ${idx + 1}`);
 
 function Spaces() {
+  const [toggle, setToggle] = useState(false);
+
+  function handleToggle() {
+    setToggle((toggled) => !toggled);
+  }
+
   return (
     <section className="flex flex-col gap-6">
       <div className="flex justify-center">
-        <Button>Joined Spaces</Button>
+        <Button onClick={handleToggle}>
+          {toggle ? 'Owned Spaces' : 'Joined Spaces'}
+        </Button>
       </div>
 
       {/* TODO: Fix flex card item alignment, not with justify-center, figure something out
@@ -23,12 +32,21 @@ function Spaces() {
           />
         ))}
 
-        <DashItem
-          styling="w-[20rem] h-[14.5rem]"
-          heading="Join Space +"
-          bgColor="bg-slate-300"
-          headingStyling="self-center my-auto"
-        />
+        {toggle ? (
+          <DashItem
+            styling="w-[20rem] h-[14.5rem]"
+            heading="Create Space +"
+            bgColor="bg-slate-300"
+            headingStyling="self-center my-auto"
+          />
+        ) : (
+          <DashItem
+            styling="w-[20rem] h-[14.5rem]"
+            heading="Join Space +"
+            bgColor="bg-slate-300"
+            headingStyling="self-center my-auto"
+          />
+        )}
       </section>
     </section>
   );
