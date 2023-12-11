c50e968faac32c7394180731bf4b0279ffcdd8c9
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-11 13:46:57 +1100

    feat: single space layout implemented

--- src/pages/Space.jsx
+++ src/pages/Space.jsx
@@ -1,3 +1,5 @@
+import DashItem from "../components/dashboard/DashItem";
+
 function Space() {
   // TODO: Add fecthing logic to dynamically display content of each space
   // Use the space ID from the url param to fetch the correct & desired space
@@ -6,8 +8,25 @@ function Space() {
   // const dynamicHeading = 'someDynamicHeading';
 
   return (
-    <section>
+    <section className="grid h-full gap-5 md:grid-cols-23 md:grid-rows-18">
+      <DashItem
+        heading="Access Code"
+        styling="col-start-1 col-end-8 row-span-5"
+      />
+
+      <DashItem heading="Capacity" styling="col-span-7 row-span-5" />
+
+      <DashItem
+        heading="Rooms"
+        styling="col-span-full col-start-[15] row-span-full row-start-1 rounded-xl"
+      />
+
+      <DashItem
+        heading="Description"
+        styling="col-start-1 col-end-[15] row-start-6 row-span-5"
+      />
       
+      <DashItem heading="Users" styling="col-start-1 col-end-[15] row-start-[11] row-span-full" />
     </section>
   );
 }
