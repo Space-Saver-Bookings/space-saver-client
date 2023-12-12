71ae0b3ea8f854f37d56db3d23909a6a53ab400d
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-13 10:40:27 +1100

    feat: implemented settings form with edit toggle functionality

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,37 @@
+import {Button} from '@mui/material';
+import SettingsForm from '../features/settings/SettingsForm';
+import {useState} from 'react';
+
 function Settings() {
-    return (
-        <div>
-            <h3>Settings</h3>
+  const [toggle, setToggle] = useState(false);
+
+  function handleToggle() {
+    setToggle((toggle) => !toggle);
+  }
+
+  return (
+    <section className="flex h-full flex-col items-center justify-center gap-8">
+      <SettingsForm disabled={!toggle} />
+
+      {toggle ? (
+        <div className='flex gap-8'>
+          <Button variant="contained" color="error">
+            Delete Account
+          </Button>
+          <Button variant='outlined' color='error' onClick={handleToggle}>
+            Cancel
+          </Button>
+          <Button variant='contained' onClick={handleToggle}>
+            Confirm
+          </Button>
         </div>
-    )
+      ) : (
+        <Button variant="contained" onClick={handleToggle}>
+          Edit
+        </Button>
+      )}
+    </section>
+  );
 }
 
-export default Settings
+export default Settings;
