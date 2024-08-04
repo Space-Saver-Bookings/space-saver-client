0f48a95c55b108e09d18d90e59694c83fc77ab4e
jordansbenjamin <14327@coderacademy.edu.au>
2024-08-04 15:09:50 +1000

    updated client setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://space-saver-server-ab36c9d46e81.herokuapp.com',
+  baseURL: 'https://spacesaver-server.vercel.app/',
 });
 
 /**
